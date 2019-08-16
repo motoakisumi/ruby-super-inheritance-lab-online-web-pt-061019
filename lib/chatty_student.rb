8683ff3b176cd9b7045460368bca71a04e6712bd
Motoaki Sumi <msumi@mba2019.hbs.edu>
2019-08-16 07:40:19 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,20 @@
+class ChattyStudent < Student
+
+  def hello
+    super
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end
+
+  def raise_hand
+    i = 0
+    loop do
+
+      super
+      puts "Pick me!"
+      i += 1
+      if i == 10
+        break
+      end
+    end
+  end
+end
